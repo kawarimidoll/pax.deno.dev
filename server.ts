673d19686accc4fcc6bb16046ca12eea2df5f0bb
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-05 21:21:13 +0900

    feat: add main feature

--- server.ts
+++ server.ts
@@ -1,9 +1,17 @@
 /// <reference path="./deploy.d.ts" />
+import { extract } from "./mod.ts";
 
 const listener = Deno.listen({ port: 8080 });
-if (!Deno.env.get("DENO_DEPLOYMENT_ID")) {
-  const { hostname, port } = listener.addr;
-  console.log(`HTTP server listening on http://${hostname}:${port}`);
+console.log(`HTTP server listening on http://localhost:${listener.addr.port}`);
+
+function genResponse(status: number, attrs?: ResponseInit): Response {
+  const statusText = {
+    400: "Invalid URL",
+    301: "Moved Permanently",
+  }[status];
+
+  const init = { status, statusText, ...attrs };
+  return new Response(`${status}: ${statusText}`, init);
 }
 
 async function handleConn(conn: Deno.Conn) {
@@ -13,22 +21,25 @@ async function handleConn(conn: Deno.Conn) {
   }
 }
 
-async function handler(request: Request, conn: Deno.Conn) {
+function handler(request: Request, _conn: Deno.Conn) {
+  console.log({ access: request.url });
   if (request.url.includes("favicon")) {
     return new Response("ok");
   }
 
-  const { href, origin, host, pathname, hash, search } = new URL(request.url);
-  console.log({ href, origin, host, pathname, hash, search });
+  const { pathname } = new URL(request.url);
 
-  const readme = await Deno.readTextFile("./README.md");
+  const [owner, repo, tag, file] = extract(pathname);
 
-  return new Response(readme, {
-    headers: {
-      "x-localaddr": `${conn.localAddr.hostname}:${conn.localAddr.port}`,
-      "x-remoteaddr": `${conn.remoteAddr.hostname}:${conn.remoteAddr.port}`,
-    },
-  });
+  if (!owner || !repo) {
+    return genResponse(400);
+  }
+
+  const host = "https://raw.githubusercontent.com";
+  const location = [host, owner, repo, tag, file].join("/");
+  console.log({ location });
+
+  return genResponse(301, { headers: { location } });
 }
 
 for await (const conn of listener) {
