0097e268e65a5fd808999b199f3afca723b4e868
GitHub <noreply@github.com>
2021-12-04 00:29:31 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 
 export { tag } from "https://deno.land/x/markup_tag@0.3.0/mod.ts";
 
